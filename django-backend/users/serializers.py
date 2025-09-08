57b90fe84f2b6c86444205c282c02b8c5dc9ea9e
Narendra Singh <Narendra@Narendra-Singh-Macbook.local>
2025-09-08 21:57:59 +0530

    Login Feature Intigrated

--- django-backend/users/serializers.py
+++ django-backend/users/serializers.py
@@ -2,7 +2,14 @@ from rest_framework import serializers
 from .models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
+    enrolled_courses = serializers.SerializerMethodField()
+
+    def get_enrolled_courses(self, obj):
+        from courses.serializers import CourseListSerializer
+        enrollments = obj.enrollments.select_related('course').all()
+        courses = [enrollment.course for enrollment in enrollments]
+        return CourseListSerializer(courses, many=True).data
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'is_student', 'is_teacher', 'phone', 'password')
+        fields = ('id', 'username', 'email', 'is_student', 'is_teacher', 'phone', 'password', 'enrolled_courses')
         extra_kwargs = {'password': {'write_only': True}}
